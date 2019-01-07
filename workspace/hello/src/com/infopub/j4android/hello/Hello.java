366012a4581db849bae7c4eb30e0842e825ff753
chanpa09 <36052882+chanpa09@users.noreply.github.com>
2019-01-08 01:36:39 +0900

    d

---
+++ workspace/hello/src/com/infopub/j4android/hello/Hello.java
@@ -0,0 +1,10 @@
+package com.infopub.j4android.hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("java4android");
+	}
+
+}

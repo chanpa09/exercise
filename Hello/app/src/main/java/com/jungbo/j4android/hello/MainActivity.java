0d45f44c718010021b4574e5c26546ebef0eebac
chanpa09 <36052882+chanpa09@users.noreply.github.com>
2019-01-23 23:19:18 +0900

    project1

--- Hello/app/src/main/java/com/jungbo/j4android/hello/MainActivity.java
+++ Hello/app/src/main/java/com/jungbo/j4android/hello/MainActivity.java
@@ -2,12 +2,44 @@ package com.jungbo.j4android.hello;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText txtbirthdate;
+    Button showbio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showComponet();
+    }
+
+    public void showComponet() {
+        txtbirthdate = (EditText) findViewById(R.id.txtBirthDate);
+        showbio = (Button) findViewById(R.id.button_today);
+
+        showbio.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == showbio) {
+            String[] _dateStrList = {"日", "月", "火", "水", "木", "金", "土"};
+            int _dt= Calendar.DAY_OF_WEEK;
+
+            String st = String.format("今日は%s曜日です。", _dateStrList[_dt-1]);
+            txtbirthdate.setText(st);
+
+            Toast.makeText(getBaseContext(), st, Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ ThermoTrans/app/src/main/java/com/jungbo/j4android/thermotrans/MainActivity.java
@@ -0,0 +1,74 @@
+package com.jungbo.j4android.thermotrans;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editCent;
+    EditText editFha;
+    RadioButton rtoFha;
+    RadioButton rtoCent;
+    Button convert;
+    Button cancell;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+        MobileAds.initialize(this, "ca-app-pub-4830018604875325~1132772017");
+
+        editCent = (EditText) this.findViewById(R.id.editCent);
+        editFha = (EditText) this.findViewById(R.id.editFha);
+        rtoFha = (RadioButton) this.findViewById(R.id.rtoFha);
+        rtoFha.setChecked(true);
+        rtoCent = (RadioButton) this.findViewById(R.id.rtoCent);
+        convert = (Button) this.findViewById(R.id.convert);
+        cancell = (Button) this.findViewById(R.id.cancell);
+
+        convert.setOnClickListener(this);
+        cancell.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        if (v == convert && rtoFha.isChecked()) {
+            double val = Double.parseDouble(editCent.getText().toString());
+            convertCentToFha(val);
+        } else if (v == convert && rtoCent.isChecked()) {
+            double val = Double.parseDouble(editFha.getText().toString());
+            convertFhaToCent(val);
+        } else if (v == cancell) {
+            String _ini = "0.0";
+            editCent.setText(_ini);
+            editFha.setText(_ini);
+        }
+    }
+
+    protected String formats(double degree) {
+        return String.format("%1$.2f", degree);
+    }
+
+    public double celsiusToFahrenheit(double c) {
+        return 9.0 / 5.0 * c + 32.0;
+    }
+
+    public double fahrenheitToCelsius(double f) {
+        return 5.0 / 9.0 * (f - 32);
+    }
+
+    protected void convertCentToFha(double val) {
+        editFha.setText(formats(celsiusToFahrenheit(val)));
+    }
+
+    protected void convertFhaToCent(double val) {
+        editCent.setText(formats(fahrenheitToCelsius(val)));
+    }
+
+}
